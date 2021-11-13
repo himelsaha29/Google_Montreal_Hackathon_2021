80d7c3868f7040ca2ab98023878aed25eb82bc09
himelsaha29 <himelsaha29@live.com>
2021-11-13 15:35:43 -0500

    Jsons loaded

---
+++ app/src/main/java/com/google/montreal/Home.java
@@ -0,0 +1,57 @@
+package com.google.montreal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.cocoahero.android.geojson.GeoJSON;
+import com.cocoahero.android.geojson.GeoJSONObject;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        String murals = loadJSONFromAsset();
+
+        TextView tv = findViewById(R.id.textview);
+
+
+
+        try {
+            GeoJSONObject geoJSON = GeoJSON.parse(murals);
+            System.out.println(geoJSON);
+            tv.setText(geoJSON.toString());
+        }
+        
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String loadJSONFromAsset() {
+        String json = null;
+        InputStream is = null;
+        try {
+            is = this.getAssets().open("murales_geojson.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+}
