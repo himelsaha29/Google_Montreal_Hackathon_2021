02515ef7d1e5bd34926a2ff5a4a35829127bda6e
himelsaha29 <himelsaha29@live.com>
2021-11-13 16:07:01 -0500

    Jsons loaded

--- app/src/main/java/com/google/montreal/Home.java
+++ app/src/main/java/com/google/montreal/Home.java
@@ -9,7 +9,9 @@ import android.widget.TextView;
 import com.cocoahero.android.geojson.GeoJSON;
 import com.cocoahero.android.geojson.GeoJSONObject;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,14 +26,16 @@ public class Home extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.textview);
 
-
-
         try {
             GeoJSONObject geoJSON = GeoJSON.parse(murals);
-            System.out.println(geoJSON);
-            tv.setText(geoJSON.toString());
+            JSONArray jsarray = geoJSON.toJSON().getJSONArray("features");
+
+            JSONObject jsobject = jsarray.getJSONObject(1);
+
+            System.out.println(jsobject);
+            tv.setText(jsobject.toString());
         }
-        
+
         catch (JSONException e) {
             e.printStackTrace();
         }
